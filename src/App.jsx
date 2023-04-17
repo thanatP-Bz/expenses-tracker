646faae0d5cb0bdffc04747a9d974f2fa521a240
Thanat <bztzn512@gmail.com>
2023-04-16 21:38:23 -0400

    expenses tracker

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import AddTransition from "./components/AddTransition";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransition />
+      </div>
+    </>
+  );
+}
+
+export default App;
