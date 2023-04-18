4c92da3b61edba2d091ca3bf517f4ea346a926b8
Thanat <bztzn512@gmail.com>
2023-04-18 17:51:26 -0400

    add transaction

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { useAppContext } from "../context/UseContext";
+import Transaction from "./Transaction";
 
 const TransactionList = () => {
+  const { transactions } = useAppContext();
+
   return (
     <>
       <h3>History</h3>
       <ul id="list" className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </>
   );
