8d48de20e44e5e6c96ad748a8b4037cdaefdc7dc
Thanat <bztzn512@gmail.com>
2023-04-17 22:32:06 -0400

    AdduseContext

---
+++ src/context/UseContext.jsx
@@ -0,0 +1,27 @@
+import React, { useContext, useReducer, createContext } from "react";
+import reducer from "./reducer";
+
+const initialState = {
+  transactions: [
+    { id: 1, text: "Flower", amount: -20 },
+    { id: 2, text: "Salary", amount: 300 },
+    { id: 3, text: "Book", amount: -10 },
+    { id: 4, text: "Camera", amount: 150 },
+  ],
+};
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
+};
+
+const useAppContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider, useAppContext };
