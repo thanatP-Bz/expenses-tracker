3362b3fafba89a8ef2dc9aeb8011d174c62cdabb
Thanat <bztzn512@gmail.com>
2023-04-21 13:03:02 -0400

    setBackend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./connectDB/connectDB.js";
+import expensesRoute from "./routes/expensesRoute.js";
+
+dotenv.config();
 const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//route
+app.use("/api/v1/expense", expensesRoute);
 
 app.get("/", (req, res) => {
   res.send("testing...");
@@ -7,4 +20,13 @@ app.get("/", (req, res) => {
 
 const PORT = 5000;
 
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}....`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () =>
+      console.log(`Server is listening on port ${PORT}....`)
+    );
+  } catch (error) {}
+};
+
+start();
